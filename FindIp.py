9b31423653c3034d1864a0ca97d78615ede0d4a6
madjo <madjo85@yandex.ru>
2017-01-13 16:23:01 +0300

    ver 1.2 added gui

--- FindIp.py
+++ FindIp.py
@@ -1,17 +1,15 @@
 import subprocess as sp
 import time
 
-output = []
-proc = sp.Popen([r'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe',
-	'GET-WMIOBJECT -Class Win32_ComputerSystem -Computer',
-	'"localhost"',
-	'|  select-Object Username'],
-	stdout = sp.PIPE)
-# output = proc.communicate()
-time.sleep(3)
-proc.kill()
-output = proc.communicate()
-print(output)
-#print(Strip(proc.communicate()))
-
-
+def Run_command(ip):
+	output = []
+	proc = sp.Popen([r'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe',
+		'GET-WMIOBJECT -Class Win32_ComputerSystem -Computer',
+		'"' + ip +'"',
+		'|  select-Object Username'],
+		stdout = sp.PIPE)
+	time.sleep(3)
+	proc.kill()
+	output = proc.communicate()
+	# print(output)
+	return output
---
+++ gui.py
@@ -0,0 +1,35 @@
+from tkinter import *
+
+class Application(Frame):
+	"""docstring for Application"""
+	def __init__(self, master):
+		super(Application, self).__init__(master)
+		self.grid(row=2,column=2)
+		self.create_widgets()
+
+
+	def create_widgets(self):
+		self.lbl_path_utc = Label(self, text = 'введите ip адресс ')
+		self.lbl_path_utc.grid(row=2,column=0,columnspan=4,sticky=W)
+		self.ent_utc = Entry(self,width = 55)
+		self.ent_utc.grid(row = 3, column = 0,columnspan=2,sticky = W)
+
+		self.lbl_uname = Label(self, text = 'имя пользователя:')
+		self.lbl_uname.grid(row=4,column=0,columnspan=4,sticky=W)
+		self.ent_uname = Entry(self,width = 55)
+		self.ent_uname.grid(row = 5, column = 0,columnspan=2,sticky = W)
+		
+		self.ok_bttn = Button(self, text = "Save", command = self.saveTofile)
+		self.ok_bttn.grid(row = 15, column = 0, sticky= W)
+
+	def saveTofile(self):
+		import FindIp
+		fp = FindIp.Run_command(self.ent_utc.get())
+
+		self.ent_uname.insert(0, fp)
+
+root = Tk()
+root.title("hellow!!")
+root.geometry("500x500")
+app = Application(root)
+root.mainloop()
