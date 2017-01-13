f19d39f763b12fde2fce124df589d5b08164f8c6
madjo <madjo85@yandex.ru>
2017-01-13 15:38:56 +0300

    ver 1.1 working powershellscript

--- FindIp.py
+++ FindIp.py
@@ -1,2 +1,17 @@
 import subprocess as sp
+import time
+
+output = []
+proc = sp.Popen([r'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe',
+	'GET-WMIOBJECT -Class Win32_ComputerSystem -Computer',
+	'"localhost"',
+	'|  select-Object Username'],
+	stdout = sp.PIPE)
+# output = proc.communicate()
+time.sleep(3)
+proc.kill()
+output = proc.communicate()
+print(output)
+#print(Strip(proc.communicate()))
+
 
